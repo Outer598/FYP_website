be1edbd2f3faa9b994751031d8646c783a66d645
FA <fisayoaasa@gmail.com>
2025-01-10 15:04:36 +0100

    Added add, delete, edit functionality

--- static/js/category.js
+++ static/js/category.js
@@ -13,20 +13,49 @@ $(document).ready(function(){
         });
     });
 
-    $(".sidebar").on("click", function(){
-        $(".table").toggleClass("widen");
-    });
-
-    $(".edit").each(function(){
+    $(".sidebar-toggler, .sidebar-menu-button").each(function(){
         $(this).on("click", function(){
-            $(".edit-add").removeClass("display-none");
-            $(".table").addClass("opac");
+            $(".table").toggleClass("widen");
         });
     });
-    $("#cancel").on("click", function(e){
+    
+
+    $(".edit").on("click", function(){
+        const row = $(this).closest("tr");
+        // console.log(row.text());
+
+        const getColumnData = (index) => {
+            return row.find('td').eq(index).text();
+        };
+        
+        $(".edit-cat").removeClass("display-none");
+        $(".table").addClass("opac");
+
+        $(".edit-cat #category_name").attr("value", `${getColumnData(1)}`);
+        
+    });
+
+    $("#edit-cancel, #add-cancel, #delete-cancel").on("click", function(e){
         e.preventDefault();
 
-        $(".edit-add").addClass("display-none");
+        $(".edit-cat, .add-cat, .delete-cat").addClass("display-none");
         $(".table").removeClass("opac");
     })
+
+    $(".table-head .head button").on("click", function(){
+        $(".add-cat").removeClass("display-none");
+        $(".table").addClass("opac");
+    });
+
+    $(".delete").on("click", function(){
+        const row = $(this).closest("tr");
+
+        const getColumnData = (index) => {
+            return row.find('td').eq(index).text();
+        };
+
+        $(".delete-cat h2").html(`Are you sure you want to Delete: ${getColumnData(1)}?`);
+        $(".delete-cat").removeClass("display-none");
+        $(".table").addClass("opac");
+    });
 })
