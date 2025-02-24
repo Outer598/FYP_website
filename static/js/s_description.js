cdcafbd8ebc4014226cf642f4f8b597de69edf82
Aasa Olufisayo <fisayoaasa@gmail.com>
2025-02-24 13:17:56 +0100

    added update functionality

--- static/js/s_description.js
+++ static/js/s_description.js
@@ -32,29 +32,63 @@ $(document).ready(function(){
         console.log(trigger);
 
         if(trigger === 'Product'){
-            $(".sections nav ol li .product span").addClass('active');
-            $(".sections nav ol li .invoice span").removeClass('active');
-            $(".sections nav ol li .receipt span").removeClass('active');
+            $(".sections nav ol li .product-header span").addClass('active');
+            $(".sections nav ol li .invoice-header span").removeClass('active');
+            $(".sections nav ol li .receipt-header span").removeClass('active');
 
             $('.productsec').removeClass('display');
             $('.invoicesec').addClass('display');
             $('.receiptsec').addClass('display');
         } else if(trigger === 'Invoice'){
-            $(".sections nav ol li .invoice span").addClass('active');
-            $(".sections nav ol li .product span").removeClass('active');
-            $(".sections nav ol li .receipt span").removeClass('active');
+            $(".sections nav ol li .invoice-header span").addClass('active');
+            $(".sections nav ol li .product-header span").removeClass('active');
+            $(".sections nav ol li .receipt-header span").removeClass('active');
 
             $('.productsec').addClass('display');
             $('.invoicesec').removeClass('display');
             $('.receiptsec').addClass('display');
         } else if(trigger === 'Receipt'){
-            $(".sections nav ol li .receipt span").addClass('active');
-            $(".sections nav ol li .invoice span").removeClass('active');
-            $(".sections nav ol li .product span").removeClass('active');
+            $(".sections nav ol li .receipt-header span").addClass('active');
+            $(".sections nav ol li .invoice-header span").removeClass('active');
+            $(".sections nav ol li .product-header span").removeClass('active');
 
             $('.productsec').addClass('display');
             $('.invoicesec').addClass('display');
             $('.receiptsec').removeClass('display');
         }
-    })
+    });
+
+    let actor = ''
+    $(".header div button, div .header-item button").on("click", function(){
+        actor = $(this).parent().attr('class');
+        
+        if (actor === 'header-item'){
+            actor = $(this).parent().parent().attr('class');
+        }
+        const currentValue = $(`.${actor} .supplierName, .${actor} .supplieremail, .${actor} .phoneNo, .${actor} .company`).text();
+        
+        
+        $(".edit .edit-items label").text(`${actor}: `);
+        $('.edit #edit').attr('value', `${currentValue}`);
+        $(".edit").removeClass("display");
+    });
+
+    $(document).on('click', '.productsec .actions .message-button', function(){
+        $('.email').removeClass('display');
+    });
+
+    $(document).on('click', '.productsec .actions .reassign-button', function(){
+        $('.reassign').removeClass('display');
+    });
+    
+    $(document).on('click', '.receiptsec .head-section .special .add-button', function(){
+        $('.receipt').removeClass('display');
+    });
+
+    $(document).on('click', '.email .email-actions .cancel, .receipt .receipt-actions .cancel, .reassign .reassign-actions .cancel, .delete .delete-actions .submit, .edit .edit-actions .cancel', function(){
+        $('.email').addClass('display');
+        $('.reassign').addClass('display');
+        $('.receipt').addClass('display');
+        $(".edit").addClass("display");
+    });
 })
